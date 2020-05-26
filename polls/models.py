f3fe60a09bbe8ef4a20f2df8764d34362aa06cb0
SunYX <15517272979@163.com>
2020-05-26 17:28:26 +0800

    third commit

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.question_text
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
